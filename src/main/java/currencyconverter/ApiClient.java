6038fb6e6f516de606307206bd92c18e9c03ef68
gilberto.italo <gilberto_italo@hotmail.com>
2024-10-09 11:46:40 +0100

    apiClient config and add env

--- src/main/java/currencyconverter/ApiClient.java
+++ src/main/java/currencyconverter/ApiClient.java
@@ -2,10 +2,23 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public class ApiClient {
     private static final String API_URL = "https://api.exchangeratesapi.io/latest";
-    private static final String API_KEY = ""; // Insira a chave da API
+    private static final String apikey;
+
+    public ApiClient() {
+        Properties properties = new Properties();
+        try {
+            properties.load(new FileInputStream("config.properties"));
+            this.apiKey = properties.getProperty("api.key");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public String getRates(String baseCurrency) {
         String url = API_URL + "?base=" + baseCurrency + "&access_key=" + API_KEY;
